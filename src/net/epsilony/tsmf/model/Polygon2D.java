2be2da712f7b96eeb6f72a5cf3f933016702ad3d
Man YUAN <epsilonyuan@gmail.com>
2012-12-21 12:02:58 +0800

    Add ray crossing algorithm impl in Polygon

--- src/net/epsilony/tsmf/model/Polygon2D.java
+++ src/net/epsilony/tsmf/model/Polygon2D.java
@@ -50,4 +50,59 @@ public class Polygon2D {
         }
         return new Polygon2D(nodeChains);
     }
+
+    /**
+     *  Originate from:<\br>
+     *       Joseph O'Rourke, Computational Geometry in C,2ed. Page 244, Code 7.13
+     * @param x
+     * @param y
+     * @return 'i' : inside , 'o' : outside, 'e' on an edge, 'v' on a vertex
+     */
+    public char rayCrossing(double x, double y) {
+        int rCross = 0, lCross = 0;
+        for (Segment2D chainHead : chains) {
+            Boundary2D seg = chainHead;
+            do {
+                Node head = seg.getHead();
+                double x1 = head.coord[0];
+                double y1 = head.coord[1];
+                if (x1 == x && y1 == y) {
+                    return 'v';
+                }
+                Node rear = seg.getRear();
+                double x2 = rear.coord[0];
+                double y2 = rear.coord[1];
+
+                boolean rStrad = (y1 > y) != (y2 > y);
+                boolean lStrad = (y1 < y) != (y2 < y);
+
+                if (rStrad || lStrad) {
+                    if (rStrad && x1 > x && x2 > x) {
+                        rCross += 1;
+                    } else if (lStrad && x1 < x && x2 < x) {
+                        lCross += 1;
+                    } else {
+                        double xCross = (x1 * y - x1 * y2 - x2 * y + x2 * y1) / (y1 - y2);
+                        if (rStrad && xCross > x) {
+                            rCross++;
+                        }
+                        if (lStrad && xCross < x) {
+                            lCross++;
+                        }
+                    }
+                }
+                seg = seg.succ;
+            } while (seg != chainHead);
+        }
+        rCross %= 2;
+        lCross %= 2;
+        if (rCross != lCross) {
+            return 'e';
+        }
+        if (rCross == 1) {
+            return 'i';
+        } else {
+            return 'o';
+        }
+    }
 }
