ef7d8b0fcbb3338d05cc22ef260d2acafdf9654e
Man YUAN <epsilonyuan@gmail.com>
2012-12-26 17:03:17 +0800

    Make Polygon2D iterable of Segment2D
     - Thus, rayCrossing() and distanceFunc() iterate by "this"
     - Pass test by distanceFunc() test unit
       + It's logically includes rayCrossing and Iterable<Segment2D> implementation.

--- src/net/epsilony/tsmf/model/Polygon2D.java
+++ src/net/epsilony/tsmf/model/Polygon2D.java
@@ -5,13 +5,14 @@
 package net.epsilony.tsmf.model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
  *
  * @author Man YUAN <epsilonyuan@gmail.com>
  */
-public class Polygon2D {
+public class Polygon2D implements Iterable<Segment2D> {
 
     ArrayList<Segment2D> chains;
 
@@ -19,7 +20,7 @@ public class Polygon2D {
         if (nodeChains.isEmpty()) {
             throw new IllegalArgumentException("There is at least 1 chain in a Polygon");
         }
-        chains=new ArrayList<>(nodeChains.size());
+        chains = new ArrayList<>(nodeChains.size());
         for (List< ? extends Node> nds : nodeChains) {
             if (nds.size() < 3) {
                 throw new IllegalArgumentException(String.format("Each chain in a polygon must contain at least 3 nodes as vertes!%n nodesChain[%d] has only %d nodes", nodeChains.indexOf(nds), nds.size()));
@@ -61,39 +62,35 @@ public class Polygon2D {
      */
     public char rayCrossing(double x, double y) {
         int rCross = 0, lCross = 0;
-        for (Segment2D chainHead : chains) {
-            Boundary2D seg = chainHead;
-            do {
-                Node head = seg.getHead();
-                double x1 = head.coord[0];
-                double y1 = head.coord[1];
-                if (x1 == x && y1 == y) {
-                    return 'v';
-                }
-                Node rear = seg.getRear();
-                double x2 = rear.coord[0];
-                double y2 = rear.coord[1];
+        for (Segment2D seg : this) {
+            Node head = seg.getHead();
+            double x1 = head.coord[0];
+            double y1 = head.coord[1];
+            if (x1 == x && y1 == y) {
+                return 'v';
+            }
+            Node rear = seg.getRear();
+            double x2 = rear.coord[0];
+            double y2 = rear.coord[1];
 
-                boolean rStrad = (y1 > y) != (y2 > y);
-                boolean lStrad = (y1 < y) != (y2 < y);
+            boolean rStrad = (y1 > y) != (y2 > y);
+            boolean lStrad = (y1 < y) != (y2 < y);
 
-                if (rStrad || lStrad) {
-                    if (rStrad && x1 > x && x2 > x) {
-                        rCross += 1;
-                    } else if (lStrad && x1 < x && x2 < x) {
-                        lCross += 1;
-                    } else {
-                        double xCross = (x1 * y - x1 * y2 - x2 * y + x2 * y1) / (y1 - y2);
-                        if (rStrad && xCross > x) {
-                            rCross++;
-                        }
-                        if (lStrad && xCross < x) {
-                            lCross++;
-                        }
+            if (rStrad || lStrad) {
+                if (rStrad && x1 > x && x2 > x) {
+                    rCross += 1;
+                } else if (lStrad && x1 < x && x2 < x) {
+                    lCross += 1;
+                } else {
+                    double xCross = (x1 * y - x1 * y2 - x2 * y + x2 * y1) / (y1 - y2);
+                    if (rStrad && xCross > x) {
+                        rCross++;
+                    }
+                    if (lStrad && xCross < x) {
+                        lCross++;
                     }
                 }
-                seg = seg.succ;
-            } while (seg != chainHead);
+            }
         }
         rCross %= 2;
         lCross %= 2;
@@ -115,16 +112,52 @@ public class Polygon2D {
         }
 
         double inf = Double.POSITIVE_INFINITY;
-        for (Segment2D cHead : chains) {
-            Segment2D seg = cHead;
-            do {
-                double dst = seg.distanceTo(x, y);
-                if (dst < inf) {
-                    inf = dst;
-                }
-                seg = (Segment2D) seg.succ;
-            } while (seg != cHead);
+        for (Segment2D seg : this) {
+            double dst = seg.distanceTo(x, y);
+            if (dst < inf) {
+                inf = dst;
+            }
         }
         return rayCrs == 'i' ? inf : -inf;
+    }
+
+    @Override
+    public Iterator<Segment2D> iterator() {
+        return new SegmentIterator();
+    }
+
+    class SegmentIterator implements Iterator<Segment2D> {
+
+        int chainId = 0;
+        Segment2D seg = chains.isEmpty() ? null : chains.get(0);
+        Segment2D last;
+
+        @Override
+        public boolean hasNext() {
+            return chainId < chains.size();
+        }
+
+        @Override
+        public Segment2D next() {
+            Segment2D res = seg;
+            seg = (Segment2D) seg.succ;
+            if (seg == chains.get(chainId)) {
+                chainId++;
+                if (chainId < chains.size()) {
+                    seg = chains.get(chainId);
+                }
+            }
+            last = res;
+            return res;
+        }
+
+        @Override
+        public void remove() {
+            if(last.pred.pred==last.succ){
+                throw new IllegalStateException("The chain is only a triangle, and no segments can be removed!");
+            }
+            last.pred.succ = last.succ;
+            last.succ.pred = last.pred;
+        }
     }
 }
