b07bfea49dda6ebc570aa028d013e954e9076235
Man YUAN <epsilonyuan+mb@gmail.com>
2012-12-26 20:40:56 +0800

    Add a more complex test sample of Polygon2D
     - The samplePolygon is added in TestTool (temporarily)
     - Test in junit test of distanceFunc()

--- src/net/epsilony/tsmf/util/TestTool.java
+++ src/net/epsilony/tsmf/util/TestTool.java
@@ -6,6 +6,7 @@ package net.epsilony.tsmf.util;
 
 import java.util.List;
 import java.util.Random;
+import net.epsilony.tsmf.model.Polygon2D;
 
 /**
  *
@@ -33,5 +34,52 @@ public class TestTool {
             ri = rd.nextInt(size);
             list.add(ri, e);
         }
+    }
+
+    public static Polygon2D samplePolygon(List<double[][][]> coordChainsOut) {
+        double[][][] coordChains = new double[][][]{{
+                {0, 0},//0
+                {2, 0},//1
+                {2, 2},//2
+                {3, 1},//3
+                {4, 2},//4
+                {5, 2},//5
+                {3, 3},//6
+                {3, 2},//7
+                {2, 4},//8
+                {6, 3},//9
+                {7, 4},//10
+                {7, 2},//11
+                {8, 5},//12
+                {8, 7},//13
+                {7, 7},//14
+                {8, 8},//15
+                {5, 9},//16
+                {6, 6},//17
+                {5, 7},//18
+                {4, 6},//19
+                {4, 8},//20
+                {3, 7},//21
+                {2, 7},//22
+                {3, 6},//23
+                {4, 4},//24
+                {0, 7},//25
+                {2, 3},//26
+                {0, 2}//27
+            },
+            {
+                {5, 5},
+                {6, 4},
+                {5, 4}
+            },
+            {
+                {7.5, 5},
+                {7, 5},
+                {6.5, 6},
+                {7.5, 6}}
+        };
+        coordChainsOut.clear();
+        coordChainsOut.add(coordChains);
+        return Polygon2D.byCoordChains(coordChains);
     }
 }
--- test/net/epsilony/tsmf/model/Polygon2DTest.java
+++ test/net/epsilony/tsmf/model/Polygon2DTest.java
@@ -4,12 +4,14 @@
  */
 package net.epsilony.tsmf.model;
 
-import org.junit.Test;
+import java.util.ArrayList;
+import net.epsilony.tsmf.util.TestTool;
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  *
- * @author epsilon
+ * @author Man YUAN <epsilonyuan@gmail.com>
  */
 public class Polygon2DTest {
 
@@ -17,13 +19,34 @@ public class Polygon2DTest {
     }
 
     @Test
-    public void testDistanceFunc() {
+    public void testDistanceFuncSimp() {
         Polygon2D pg = samplePolygon();
-        double[][] testCoords = new double[][]{{0.5, 0.5}, {0.1, 0.1}, {-0.1, -0.1}, {0.5, 0.4},{0.5,0.25}};
-        double[] exps = new double[]{0, 0.1, -0.1 * Math.sqrt(2), 0.1,0.25};
+        double[][] testCoords = new double[][]{{0.5, 0.5}, {0.1, 0.1}, {-0.1, -0.1}, {0.5, 0.4}, {0.5, 0.25}};
+        double[] exps = new double[]{0, 0.1, -0.1 * Math.sqrt(2), 0.1, 0.25};
         for (int i = 0; i < exps.length; i++) {
             double exp = exps[i];
             double act = pg.distanceFunc(testCoords[i][0], testCoords[i][1]);
+            assertEquals(exp, act, 1e-12);
+        }
+    }
+
+    @Test
+    public void testDistanceFuncComp() {
+        ArrayList<double[][][]> coords = new ArrayList<>(1);
+        Polygon2D pg = TestTool.samplePolygon(coords);
+        double[][] testCoordsExps = new double[][]{{
+                3, 6.5, Math.sqrt(2) / 4},
+            {7, 5.5, -Math.sqrt(5) / 10},
+            {-1, -1, -Math.sqrt(2)},
+            {1, 2.5, 0},
+            {6.25, 6, 0.25},
+            {2.9, 3, -0.1}
+        };
+        for (double[] xy_exp : testCoordsExps) {
+            double exp = xy_exp[2];
+            double x = xy_exp[0];
+            double y = xy_exp[1];
+            double act = pg.distanceFunc(x, y);
             assertEquals(exp, act, 1e-12);
         }
     }
