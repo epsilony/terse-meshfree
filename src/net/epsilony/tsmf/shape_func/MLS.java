348a8e372af83d7fb19a0773b9d1dbc9777e4b90
Man YUAN <epsilonyuan+mb@gmail.com>
2013-01-11 20:56:54 +0800

    Implements MLS (moving least square) and pass the test

---
+++ src/net/epsilony/tsmf/shape_func/MLS.java
@@ -0,0 +1,225 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.epsilony.tsmf.shape_func;
+
+import gnu.trove.list.array.TDoubleArrayList;
+import java.util.List;
+import net.epsilony.tsmf.util.Math2D;
+import net.epsilony.tsmf.util.WithDiffOrder;
+import net.epsilony.tsmf.util.WithDiffOrderUtil;
+import org.ejml.alg.dense.decomposition.lu.LUDecompositionNR;
+import org.ejml.alg.dense.linsol.lu.LinearSolverLu;
+import org.ejml.alg.dense.mult.MatrixVectorMult;
+import org.ejml.data.DenseMatrix64F;
+import org.ejml.ops.CommonOps;
+
+/**
+ *
+ * @author epsilon
+ */
+public class MLS implements WithDiffOrder {
+
+    RadialFunction2D weightFunc;
+    BasisFunction basisFunc;
+    DenseMatrix64F[] matAs;
+    public final int DEFAULT_MAT_B_CAPACITY = 50;
+    TDoubleArrayList[][] matBs;
+    TDoubleArrayList[] distsCache;
+    TDoubleArrayList[] weightsCache;
+    TDoubleArrayList[] basisCache;
+    TDoubleArrayList commonCache;
+    LinearSolverLu solver = new LinearSolverLu(new LUDecompositionNR());
+    //DenseMatrix64F solverA;
+    DenseMatrix64F b_;
+    DenseMatrix64F gamma, gamma_d, tv1, tv2;
+    static private final double[] ZERO = new double[]{0, 0};
+
+    @Override
+    public int getDiffOrder() {
+        return weightFunc.getDiffOrder();
+    }
+
+    @Override
+    public void setDiffOrder(int diffOrder) {
+        if (diffOrder < 0 || diffOrder > 1) {
+            throw new IllegalArgumentException("only support diffOrder that is 0 or 1, not " + diffOrder);
+        }
+        weightFunc.setDiffOrder(diffOrder);
+        buildCaches();
+    }
+
+    private void buildCaches() {
+        int diffSize = WithDiffOrderUtil.outputLength2D(getDiffOrder());
+        matAs = new DenseMatrix64F[diffSize];
+        distsCache = new TDoubleArrayList[diffSize];
+        basisCache = new TDoubleArrayList[diffSize];
+
+        int basisLen = basisFunc.basisLength();
+        weightsCache = new TDoubleArrayList[diffSize];
+        gamma = new DenseMatrix64F(basisLen, 1);
+        gamma_d = new DenseMatrix64F(basisLen, 1);
+        tv1 = new DenseMatrix64F(basisLen, 1);
+        tv2 = new DenseMatrix64F(basisLen, 1);
+        b_ = new DenseMatrix64F(basisLen, 1);
+        commonCache = new TDoubleArrayList(DEFAULT_MAT_B_CAPACITY);
+        for (int i = 0; i < diffSize; i++) {
+            matAs[i] = new DenseMatrix64F(basisLen, basisLen);
+            distsCache[i] = new TDoubleArrayList(DEFAULT_MAT_B_CAPACITY);
+            weightsCache[i] = new TDoubleArrayList(DEFAULT_MAT_B_CAPACITY);
+            basisCache[i] = new TDoubleArrayList(basisLen);
+        }
+        matBs = new TDoubleArrayList[diffSize][basisLen];
+        for (int i = 0; i < matBs.length; i++) {
+            TDoubleArrayList[] matB = matBs[i];
+            for (int j = 0; j < matB.length; j++) {
+                matB[j] = new TDoubleArrayList(DEFAULT_MAT_B_CAPACITY);
+            }
+        }
+    }
+
+    private void resetCaches(int capacity) {
+        for (int i = 0; i < distsCache.length; i++) {
+            distsCache[i].resetQuick();
+            distsCache[i].ensureCapacity(capacity);
+            matAs[i].zero();
+            TDoubleArrayList[] bMat = matBs[i];
+            for (int j = 0; j < bMat.length; j++) {
+                bMat[j].resetQuick();
+                bMat[j].ensureCapacity(capacity);
+            }
+        }
+        commonCache.resetQuick();
+        commonCache.ensureCapacity(capacity);
+    }
+
+    public MLS(RadialFunction2D weightFunc, BasisFunction basisFunc) {
+        this.weightFunc = weightFunc;
+        this.basisFunc = basisFunc;
+        weightFunc.setDiffOrder(0);
+        buildCaches();
+    }
+
+    public MLS(RadialFunction2D weightFunc) {
+        this.weightFunc = weightFunc;
+        this.basisFunc = new MonomialBasis2D(2);
+        weightFunc.setDiffOrder(0);
+        buildCaches();
+    }
+
+    public MLS() {
+        this.weightFunc = new RadialFunction2D();
+        this.basisFunc = new MonomialBasis2D(2);
+        weightFunc.setDiffOrder(0);
+        buildCaches();
+    }
+
+    public TDoubleArrayList[] values(double[] xy, List<double[]> coords, TDoubleArrayList influcenceRads, TDoubleArrayList[] dists, TDoubleArrayList[] output) {
+        resetCaches(coords.size());
+        TDoubleArrayList[] results = WithDiffOrderUtil.initOutput(output, coords.size(), 2, getDiffOrder());
+        TDoubleArrayList[] ds = dists;
+        if (null == dists) {
+            fillDistsCache(xy, coords);
+            ds = distsCache;
+        }
+        weightFunc.values(ds, influcenceRads, weightsCache);
+
+        basisFunc.setDiffOrder(0);
+        double[] tds = new double[2];
+        int crd_index = 0;
+        for (double[] crd : coords) {
+            basisFunc.values(Math2D.subs(crd,xy, tds), basisCache);
+            TDoubleArrayList basis = basisCache[0];
+            for (int i = 0; i < matAs.length; i++) {
+                DenseMatrix64F A_d = matAs[i];
+                TDoubleArrayList[] B_d = matBs[i];
+                double weight = weightsCache[i].getQuick(crd_index);
+                addToA(weight, basis, A_d);
+                addToB(weight, basis, B_d);
+            }
+            crd_index++;
+        }
+
+        int diffOrder = getDiffOrder();
+        if (diffOrder > 0) {
+            basisFunc.setDiffOrder(diffOrder);
+        }
+        basisFunc.values(ZERO, basisCache);
+        solver.setA(matAs[0]);
+        copyTo(basisCache[0], b_);
+        solver.solve(b_, gamma);
+        dot(matBs[0], gamma, results[0]);
+
+        if (diffOrder > 0) {
+            for (int i = 1; i < matAs.length; i++) {
+                MatrixVectorMult.mult(matAs[i], gamma, tv1);
+                copyTo(basisCache[i], tv2);
+                CommonOps.sub(tv2, tv1, tv1);
+                solver.solve(tv1, gamma_d);
+                dot(matBs[i], gamma, results[i]);
+                commonCache.resetQuick();
+                dot(matBs[0], gamma_d, commonCache);
+                addTo(commonCache, results[i]);
+            }
+        }
+
+        return results;
+    }
+
+    static private void addTo(TDoubleArrayList from, TDoubleArrayList to) {
+        for (int i = 0; i < from.size(); i++) {
+            to.setQuick(i, to.getQuick(i) + from.getQuick(i));
+        }
+    }
+
+    static private void dot(TDoubleArrayList[] mat, DenseMatrix64F vec, TDoubleArrayList result) {
+        for (int i = 0; i < mat[0].size(); i++) {
+            double sum = 0;
+            for (int j = 0; j < mat.length; j++) {
+                sum += vec.get(j) * mat[j].getQuick(i);
+            }
+            result.add(sum);
+        }
+    }
+
+    static private void copyTo(TDoubleArrayList from, DenseMatrix64F to) {
+        for (int i = 0; i < from.size(); i++) {
+            to.set(i, from.getQuick(i));
+        }
+    }
+
+    static private void addToA(double w, TDoubleArrayList basis, DenseMatrix64F matA) {
+        for (int i = 0; i < basis.size(); i++) {
+            double b1 = basis.getQuick(i);
+            for (int j = 0; j < basis.size(); j++) {
+                double b2 = basis.getQuick(j);
+                matA.add(i, j, w * b1 * b2);
+            }
+        }
+    }
+
+    static private void addToB(double w, TDoubleArrayList basis, TDoubleArrayList[] matB) {
+        for (int i = 0; i < basis.size(); i++) {
+            matB[i].add(w * basis.getQuick(i));
+        }
+    }
+
+    private void fillDistsCache(double[] xy, List<double[]> coords) {
+        int diffSize = distsCache.length;
+        double[] tds = null;
+        if (diffSize > 1) {
+            tds = new double[diffSize];
+        }
+        for (double[] crd : coords) {
+            if (diffSize > 1) {
+                Math2D.distanceAndPartDiffs(xy, crd, tds);
+                for (int i = 0; i < distsCache.length; i++) {
+                    distsCache[i].add(tds[i]);
+                }
+            } else {
+                distsCache[0].add(Math2D.distance(xy, crd));
+            }
+        }
+    }
+}
---
+++ test/net/epsilony/tsmf/shape_func/MLSTest.java
@@ -0,0 +1,180 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.epsilony.tsmf.shape_func;
+
+import gnu.trove.list.array.TDoubleArrayList;
+import static java.lang.Math.PI;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import net.epsilony.tsmf.util.Math2D;
+import net.epsilony.tsmf.util.TestTool;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author epsilon
+ */
+public class MLSTest {
+
+    public MLSTest() {
+    }
+    double[] sampleInterval = new double[]{1, 4};
+
+    LinkedList<double[]> genSampleCoords(int numPerDim) {
+        double[] v = TestTool.linSpace(sampleInterval[0], sampleInterval[1], numPerDim);
+        LinkedList<double[]> coords = new LinkedList<>();
+        for (double x : v) {
+            for (double y : v) {
+                coords.add(new double[]{x, y});
+            }
+        }
+        return coords;
+    }
+
+    public double[] polynomialSample(double[] xy) {
+        double x = xy[0], y = xy[1];
+        return new double[]{
+                    1.1 + 3 * x + 4.4 * y - x * x - 1.3 * x * y + 2.2 * y * y,
+                    3 - 2 * x - 1.3 * y,
+                    4.4 - 1.3 * x + 4.4 * y
+                };
+    }
+
+    public double[] sin_cos_sample(double[] xy) {
+        double cycle = 40;
+        double par = 2 * PI / cycle;
+        double x = xy[0], y = xy[1];
+        double val = sin(x * par) * cos(y * par);
+        double val_x = par * cos(x * par) * cos(y * par);
+        double val_y = -par * sin(x * par) * sin(y * par);
+        return new double[]{val, val_x, val_y};
+    }
+
+    public static interface SampFunc {
+
+        double[] val(double[] xy);
+    }
+    SampFunc[] funcs = new SampFunc[]{
+        new SampFunc() {
+            @Override
+            public double[] val(double[] xy) {
+                return polynomialSample(xy);
+            }
+        },
+        new SampFunc() {
+            @Override
+            public double[] val(double[] xy) {
+                return sin_cos_sample(xy);
+            }
+        }
+    };
+
+    double[] randomRads(double rad, double range, int num) {
+        Random rand = new Random();
+        double[] res = new double[num];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = rad + range * rand.nextDouble();
+        }
+        return res;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testPartionOfUnity() {
+
+        int numPerDim = 50;
+        int testPerDim = 7;
+        double rad_avg = (sampleInterval[1] - sampleInterval[0]) / numPerDim * 4;
+        double range = rad_avg * 0.2;
+        MLS mls = new MLS();
+        mls.setDiffOrder(1);
+        LinkedList<double[]> coords = genSampleCoords(numPerDim);
+        LinkedList<double[]> testPts = genSampleCoords(testPerDim);
+        TDoubleArrayList[] radss = new TDoubleArrayList[]{new TDoubleArrayList(new double[]{rad_avg}), new TDoubleArrayList(randomRads(rad_avg, range, coords.size()))};
+        double[] exp = new double[]{1, 0, 0};
+        for (TDoubleArrayList rads : radss) {
+            for (double[] pt : testPts) {
+                Object[] searchRes = searchCoords(pt, coords, rads);
+                TDoubleArrayList[] vals = mls.values(pt, (List<double[]>) searchRes[0], (TDoubleArrayList) searchRes[1], null, null);
+                double[] acts = new double[]{vals[0].sum(), vals[1].sum(), vals[2].sum()};
+                assertArrayEquals(exp, acts, 1e-12);
+
+            }
+        }
+    }
+
+    @Test
+    public void testFitness() {
+        int numPerDim = 50;
+        int testPerDim = 7;
+        double rad_avg = (sampleInterval[1] - sampleInterval[0]) / numPerDim * 4;
+        double range = rad_avg * 0.2;
+        boolean isVer = false;
+        MLS mls = new MLS();
+        mls.setDiffOrder(1);
+        LinkedList<double[]> coords = genSampleCoords(numPerDim);
+        LinkedList<double[]> testPts = genSampleCoords(testPerDim);
+        TDoubleArrayList[] radss = new TDoubleArrayList[]{new TDoubleArrayList(new double[]{rad_avg}), new TDoubleArrayList(randomRads(rad_avg, range, coords.size()))};
+        double[] errs = new double[]{1e-10, 9e-6};
+        for (TDoubleArrayList rads : radss) {
+            for (double[] pt : testPts) {
+                for (int i = 0; i < errs.length; i++) {
+                    Object[] searchRes = searchCoords(pt, coords, rads);
+                    List<double[]> res_coords = (List<double[]>) searchRes[0];
+                    TDoubleArrayList[] vals = mls.values(pt, res_coords, (TDoubleArrayList) searchRes[1], null, null);
+                    double[] acts = new double[]{0, 0, 0};
+                    int j = 0;
+                    for (double[] c : res_coords) {
+                        double cv = funcs[i].val(c)[0];
+                        acts[0] += vals[0].get(j) * cv;
+                        acts[1] += vals[1].get(j) * cv;
+                        acts[2] += vals[2].get(j) * cv;
+                        j++;
+                    }
+                    double[] exps = funcs[i].val(pt);
+                    if (isVer) {
+                        System.out.println("acts = " + Arrays.toString(acts) + "exps = " + Arrays.toString(acts));
+                    }
+                    assertArrayEquals(exps, acts, errs[i]);
+                }
+            }
+        }
+    }
+
+    public static Object[] searchCoords(double[] center, List<double[]> coords, TDoubleArrayList rads) {
+        LinkedList<double[]> res = new LinkedList<>();
+        int i = 0;
+        TDoubleArrayList res_rads = new TDoubleArrayList(100);
+        LinkedList<Integer> res_indes = new LinkedList<>();
+        boolean isUniRad = true;
+        double uniRad = rads.get(0);
+        if (rads.size() > 1) {
+            isUniRad = false;
+        }
+        for (double[] coord : coords) {
+            double rad = uniRad;
+            if (!isUniRad) {
+                rad = rads.get(i);
+            }
+            double dst = Math2D.distance(center, coord);
+            if (dst < rad) {
+                res.add(coord);
+                res_rads.add(rad);
+                res_indes.add(i);
+            }
+            i++;
+        }
+        if (isUniRad) {
+            return new Object[]{res, rads};
+        } else {
+            return new Object[]{res, res_rads, res_indes};
+        }
+    }
+}
