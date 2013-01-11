b2cdace8567d50e653d4117078f2f4d203e972f7
Man YUAN <epsilonyuan+mb@gmail.com>
2013-01-11 10:05:18 +0800

    Add interface WithDiffOrder and its util
     - also adapte trove's primity array list package as a library in Netbeans project

---
+++ src/net/epsilony/tsmf/util/WithDiffOrder.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.epsilony.tsmf.util;
+
+/**
+ *
+ * @author Man YUAN <epsilonyuan@gmail.com>
+ */
+public interface WithDiffOrder {
+
+    int getDiffOrder();
+
+    void setDiffOrder(int diffOrder);
+}
