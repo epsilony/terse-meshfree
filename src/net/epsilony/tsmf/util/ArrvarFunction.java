3ddce277824fef7dabaed7df102666b2dcaa0779
Man YUAN <epsilonyuan@gmail.com>
2013-01-16 14:16:03 +0800

    Make Interface ArrvarFunction

---
+++ src/net/epsilony/tsmf/util/ArrvarFunction.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.epsilony.tsmf.util;
+
+/**
+ *
+ * @author <a href="mailto:epsionyuan@gmail.com">Man YUAN</a>
+ */
+public interface ArrvarFunction {
+    public double value(double[] vec);
+}
