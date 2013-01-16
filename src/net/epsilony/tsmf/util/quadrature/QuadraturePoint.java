affe073969aaf2edcf385be9504ed3d82c3b7f83
Man YUAN <epsilonyuan@gmail.com>
2013-01-16 14:18:01 +0800

    Make a Symmetric Quadratue on Triangle
     - SymTriangleQuadratue
     - make package util.quadratue
     - test and pass

---
+++ src/net/epsilony/tsmf/util/quadrature/QuadraturePoint.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.epsilony.tsmf.util.quadrature;
+
+/**
+ *
+ * @author <a href="mailto:epsionyuan@gmail.com">Man YUAN</a>
+ */
+public class QuadraturePoint {
+
+    double weight;
+    double[] coord;
+
+    QuadraturePoint(double weight, double x, double y) {
+        this.weight = weight;
+        coord = new double[]{x, y};
+    }
+
+    public QuadraturePoint() {
+        coord = new double[2];
+    }
+
+    public QuadraturePoint(int dim) {
+        coord = new double[dim];
+    }
+}
