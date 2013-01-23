a2e7f5412ab8647089ffee708d3b4d80a77c945a
Man YUAN <epsilonyuan+mb@gmail.com>
2013-01-23 20:08:14 +0800

    Implement a RawConstitutiveLaw

---
+++ src/net/epsilony/tsmf/cons_law/RawConstitutiveLaw.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.epsilony.tsmf.cons_law;
+
+import no.uib.cipr.matrix.DenseMatrix;
+
+/**
+ *
+ * @author epsilon
+ */
+public class RawConstitutiveLaw implements ConstitutiveLaw{
+    boolean isSym;
+    DenseMatrix matrix;
+
+    @Override
+    public DenseMatrix getMatrix() {
+        return matrix;
+    }
+
+    @Override
+    public boolean isSymmetric() {
+        return isSym;
+    }
+
+    public RawConstitutiveLaw(boolean isSym, DenseMatrix matrix) {
+        this.isSym = isSym;
+        this.matrix = matrix;
+    }
+    
+    
+}
