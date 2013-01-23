d882fe8c227c6e5c9255ae27332123cffa3bf32b
Man YUAN <epsilonyuan+mb@gmail.com>
2013-01-23 20:30:48 +0800

    Add Segment2DQuadrature tool and test

---
+++ src/net/epsilony/tsmf/util/quadrature/Segment2DQuadrature.java
@@ -0,0 +1,92 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.epsilony.tsmf.util.quadrature;
+
+import java.util.Iterator;
+import net.epsilony.tsmf.model.Segment2D;
+import net.epsilony.tsmf.util.ArrvarFunction;
+
+/**
+ *
+ * @author epsilon
+ */
+public class Segment2DQuadrature implements Iterable<QuadraturePoint> {
+
+    Segment2D segment = null;
+    double segLen;
+    int degree;
+    double[] points;
+    double[] weights;
+
+    private void _setSegment(Segment2D segment) {
+        this.segment = segment;
+        segLen = segment.length();
+    }
+
+    public Segment2DQuadrature(Segment2D segment, int degree) {
+        _setSegment(segment);
+        _setDegree(degree);
+    }
+
+    public Segment2DQuadrature(int degree) {
+        _setDegree(degree);
+    }
+
+    private void _setDegree(int degree) {
+        double[][] pws = GaussLegendre.pointsWeightsByDegree(degree);
+        points = pws[0];
+        weights = pws[1];
+        this.degree = degree;
+    }
+
+    public void setSegment(Segment2D segment) {
+        _setSegment(segment);
+    }
+
+    public void setDegree(int degree) {
+        _setDegree(degree);
+    }
+
+    @Override
+    public Iterator<QuadraturePoint> iterator() {
+        return new MyIterator();
+    }
+
+    private class MyIterator implements Iterator<QuadraturePoint> {
+
+        int nextIdx = 0;
+
+        @Override
+        public boolean hasNext() {
+            return nextIdx < points.length;
+        }
+
+        @Override
+        public QuadraturePoint next() {
+            double weight = weights[nextIdx] / 2 * segLen;
+            double point = points[nextIdx];
+            double[] cHead = segment.getHead().coord;
+            double[] cRear = segment.getRear().coord;
+            double p = (point + 1) / 2;
+            double x = cHead[0] * (1 - p) + cRear[0] * p;
+            double y = cHead[1] * (1 - p) + cRear[1] * p;
+            nextIdx++;
+            return new QuadraturePoint(weight, x, y);
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+    }
+    
+    public double quadrate(ArrvarFunction func){
+        double res=0;
+        for (QuadraturePoint qp:this){
+            res+=func.value(qp.coord)*qp.weight;
+        }
+        return res;
+    }
+}
---
+++ test/net/epsilony/tsmf/util/quadrature/Segment2DQuadratureTest.java
@@ -0,0 +1,88 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.epsilony.tsmf.util.quadrature;
+
+import net.epsilony.tsmf.model.Node;
+import net.epsilony.tsmf.model.Segment2D;
+import net.epsilony.tsmf.util.ArrvarFunction;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author epsilon
+ */
+public class Segment2DQuadratureTest {
+
+    public Segment2DQuadratureTest() {
+    }
+
+    @Test
+    public void testLength() {
+        final double val = 1.3;
+        ArrvarFunction func = new ArrvarFunction() {
+            @Override
+            public double value(double[] vec) {
+                return val;
+            }
+        };
+
+        Segment2D seg = new Segment2D(new Node(1, -1));
+        seg.succ = new Segment2D(new Node(-2, 3));
+        double exp = 5 * val;
+        boolean getHere = false;
+        for (int deg = 1; deg < GaussLegendre.MAXPOINTS * 2 - 1; deg++) {
+            Segment2DQuadrature sq = new Segment2DQuadrature(seg, deg);
+            double act = sq.quadrate(func);
+            assertEquals(exp, act, 1e-12);
+            getHere = true;
+        }
+        assertTrue(getHere);
+    }
+    
+     @Test
+    public void testLadderX() {
+        ArrvarFunction func = new ArrvarFunction() {
+            @Override
+            public double value(double[] vec) {
+                return vec[0];
+            }
+        };
+
+        Segment2D seg = new Segment2D(new Node(1, 2));
+        seg.succ = new Segment2D(new Node(-2, 6));
+        double exp = -2.5;
+        boolean getHere = false;
+        for (int deg = 1; deg < GaussLegendre.MAXPOINTS * 2 - 1; deg++) {
+            Segment2DQuadrature sq = new Segment2DQuadrature(seg, deg);
+            double act = sq.quadrate(func);
+            assertEquals(exp, act, 1e-12);
+            getHere = true;
+        }
+        assertTrue(getHere);
+    }
+
+    @Test
+    public void testLadderY() {
+        ArrvarFunction func = new ArrvarFunction() {
+            @Override
+            public double value(double[] vec) {
+                return vec[1];
+            }
+        };
+
+        Segment2D seg = new Segment2D(new Node(1, 2));
+        seg.succ = new Segment2D(new Node(-2, 6));
+        double exp = 20;
+        boolean getHere = false;
+        for (int deg = 1; deg < GaussLegendre.MAXPOINTS * 2 - 1; deg++) {
+            Segment2DQuadrature sq = new Segment2DQuadrature(seg, deg);
+            double act = sq.quadrate(func);
+            assertEquals(exp, act, 1e-12);
+            getHere = true;
+        }
+        assertTrue(getHere);
+    }
+}
