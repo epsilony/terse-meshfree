c758e6444a321bf4657130ef4167cb175d406df3
Man YUAN <epsilonyuan+mb@gmail.com>
2013-02-19 19:37:48 +0800

    add setToZoomAll() in ModelTransform

--- src/net/epsilony/tsmf/util/ui/ModelTransform.java
+++ src/net/epsilony/tsmf/util/ui/ModelTransform.java
@@ -5,6 +5,7 @@
 package net.epsilony.tsmf.util.ui;
 
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
 
 /**
  *
@@ -54,5 +55,31 @@ public class ModelTransform extends AffineTransform {
     public void resetToDefault() {
         unitScaleAndSetOrigin(defaultOriginX, defaultOriginY);
         scale(defaultScale, defaultScale);
+    }
+
+    public void setToZoomAll(Rectangle2D modelBound, int componentWidth, int componentHeight) {
+        double modelWidth = modelBound.getWidth();
+        double modelHeight = modelBound.getHeight();
+        if (0 == modelWidth && 0 == modelHeight) {
+            setDefaultOriginAndScale(zoomAllMargin, componentHeight - zoomAllMargin, 1);
+        } else {
+            double scaleX = modelWidth > 0 ? (componentWidth - 2 * zoomAllMargin) / modelWidth : Double.POSITIVE_INFINITY;
+            double scaleY = modelHeight > 0 ? (componentHeight - 2 * zoomAllMargin) / modelHeight : Double.POSITIVE_INFINITY;
+            double scale = Math.min(scaleX, scaleY);
+            if (scale < ModelTransform.SCALE_LOWER_LIMITE) {
+                scale = ModelTransform.SCALE_LOWER_LIMITE;
+            }
+            double minXOnComponent = zoomAllMargin;
+            double maxYOnComponent = componentHeight - zoomAllMargin;
+            if (scaleX > scaleY) {
+                minXOnComponent = (componentWidth - modelWidth * scale) / 2;
+            } else if (scaleY > scaleX) {
+                maxYOnComponent = (componentHeight + modelHeight * scale) / 2;
+            }
+            double oriX = minXOnComponent - modelBound.getMinX() * scale;
+            double oriY = maxYOnComponent + modelBound.getMinY() * scale;
+            setDefaultOriginAndScale(oriX, oriY, scale);
+        }
+        resetToDefault();
     }
 }
