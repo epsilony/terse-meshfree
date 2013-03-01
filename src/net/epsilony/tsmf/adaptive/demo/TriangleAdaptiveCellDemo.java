658753047b59d6b7f88a15ecfa706969c3f2be5f
Man YUAN <epsilonyuan+mb@gmail.com>
2013-03-01 20:43:22 +0800

    Add TriangleAdaptiveCellDemo

---
+++ src/net/epsilony/tsmf/adaptive/demo/TriangleAdaptiveCellDemo.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.epsilony.tsmf.adaptive.demo;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import java.util.Arrays;
+import java.util.List;
+import javax.swing.SwingUtilities;
+import net.epsilony.tsmf.adaptive.AdaptiveCell;
+import net.epsilony.tsmf.adaptive.TriangleAdaptiveCellFactory;
+import net.epsilony.tsmf.util.ui.ModelDrawer;
+import net.epsilony.tsmf.util.ui.ModelDrawerAdapter;
+
+/**
+ *
+ * @author epsilon
+ */
+public class TriangleAdaptiveCellDemo extends AbstractAdaptiveCellDemo {
+
+    public static double TRIANGLE_EDGE_LENGTH = 20;
+    public static Rectangle2D TRIANGLE_COVERY_RANGE = new Rectangle2D.Double(5, 5, 100, 60);
+
+    @Override
+    protected AdaptiveCell[][] genCells() {
+        return TriangleAdaptiveCellFactory.coverRectangle(TRIANGLE_COVERY_RANGE, TRIANGLE_EDGE_LENGTH);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new TriangleAdaptiveCellDemo().createDemoUI();
+            }
+        });
+    }
+
+    @Override
+    protected List<ModelDrawer> getExtraDrawers() {
+        ModelDrawer drawer = new ModelDrawerAdapter() {
+            @Override
+            public Rectangle2D getBoundsInModelSpace() {
+                return null;
+            }
+
+            @Override
+            public void drawModel(Graphics2D g2) {
+                g2.setColor(Color.GRAY);
+                g2.draw(modelToComponentTransform.createTransformedShape(TRIANGLE_COVERY_RANGE));
+            }
+        };
+
+        return Arrays.asList(drawer);
+    }
+}
