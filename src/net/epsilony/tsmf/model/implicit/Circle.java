3250d1528cef85749b0c754e938b3b5e5583b47b
Man YUAN <epsilonyuan+mb@gmail.com>
2013-03-17 16:35:35 +0800

    add Circle

---
+++ src/net/epsilony/tsmf/model/implicit/Circle.java
@@ -0,0 +1,70 @@
+/* (c) Copyright by Man YUAN */
+package net.epsilony.tsmf.model.implicit;
+
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+import net.epsilony.tsmf.util.ArrvarFunction;
+import net.epsilony.tsmf.util.GenericFunction;
+import net.epsilony.tsmf.util.Math2D;
+
+/**
+ *
+ * @author <a href="mailto:epsilonyuan@gmail.com">Man YUAN</a>
+ */
+public class Circle implements ArrvarFunction, GenericFunction<double[], double[]> {
+
+    double radius;
+    double centerX, centerY;
+
+    public Circle(double centerX, double centerY, double radius) {
+        this.radius = radius;
+        this.centerX = centerX;
+        this.centerY = centerY;
+    }
+
+    public Circle() {
+    }
+
+    @Override
+    public double value(double[] vec) {
+        return radius - Math2D.distance(vec[0], vec[1], centerX, centerY);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getCenterX() {
+        return centerX;
+    }
+
+    public void setCenterX(double centerX) {
+        this.centerX = centerX;
+    }
+
+    public double getCenterY() {
+        return centerY;
+    }
+
+    public void setCenterY(double centerY) {
+        this.centerY = centerY;
+    }
+
+    Shape genShape() {
+        return new Ellipse2D.Double(centerX - radius, centerY - radius, radius * 2, radius * 2);
+    }
+
+    @Override
+    public double[] value(double[] input, double[] output) {
+        if (null == output) {
+            output = new double[]{value(input)};
+        } else {
+            output[0] = value(input);
+        }
+        return output;
+    }
+}
